89790b7812b57f70cdc5696a4569b2a279cc8295
Austin K <austinkranz@gmail.com>
2023-02-17 20:59:35 -0600

    MOAR FRIDAY FRENZY

--- DecryptStringFromAlphabetToIntegerMapping.py
+++ DecryptStringFromAlphabetToIntegerMapping.py
@@ -1,39 +1,55 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        #Scan the text
-        #If you come across a #, you know the two characters preceeding it belong to the hashtag. Make a note of their index location in the string
-        #Add the index locations to a list
-        #Scan the text again, and for each character check if it's index is in the list. If not, it belongs to the a-i character set, and can be added to the result
-        #If you find a hashtag, capture the chars that are index-1 and index-2 from it and add it to the result
-
-        indexDict = {}
-        indexCounter = 1
-        hashList = []
-        lastIndex = 0
-
-        for char in s:
-            charIndex =  s.index(char,lastIndex)
-            if char == "#":
-                if charIndex not in indexDict:
-                    indexDict[indexCounter] = charIndex #Stores the index of the #. The key is a number incrementing by 1. So at the end, there will be an entry in the dictionary of the index of every #
-                    indexCounter +=1
-                    lastIndex = charIndex
-        
-        lastIndex = 0
-        for char in s:
-            charIndex =  s.index(char,lastIndex)
-            lastIndex = charIndex
-            if charIndex+2 in indexDict: #If the index of this character (plus 2) is the index of the hashtag,we know that this char and the next belong to the hashtag
-                hashList.append(char)
-                hashList.append(s[charIndex+1]) #adds the hashtag numbers to a list
-                newString = "".join(hashList)
-                print(newString)
-        
-        print(indexDict)
             
+        nums = ["1","2","3","4","5","6","7","8","9","10#","11#","12#","13#","14#","15#","16#","17#","18#","19#","20#","21#","22#","23#","24#","25#","26#"]
+        letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        mapDict = {}
+        returnList = []
+
+        stringList = []
+        counter = 0
+        tempstring = ""
+        finalList = []
+        tempList = []
+
+        for item in s:
+            stringList.append(item)
+        
+        stringList.reverse()
+
+        for item in stringList:
+            if item == "#":
+                counter = 2
+            else:
+                if counter > 0:
+                    tempstring = tempstring +  str(item)
+                    counter -= 1
+                    if counter == 0:
+                        for digit in tempstring:
+                            tempList.append(digit)
+                        tempList.reverse()
+                        mystring = "".join(tempList)
+                        finalList.append(mystring+"#")
+                        tempstring = ""
+                        mystring = ""
+                        tempList.clear()
+                else:
+                    finalList.append(item)
+        finalList.reverse()
+        
+        for num in nums:
+            mapDict[num]=letters[nums.index(num)]
+
+        
+        for item in finalList:
+            returnList.append(mapDict[item])
+        return "".join(returnList)
+        
 
 
 
 a = Solution()
 s = "10#11#12"
 print(a.freqAlphabets(s))
+
+#reverse = 21#11#01
