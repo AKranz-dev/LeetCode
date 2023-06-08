1033528e7def9ff88da4ce7315395fabe44d3d2e
Austin K <austinkranz@gmail.com>
2023-06-08 16:51:03 -0500

    CountNegativeNumbersinaSortedMatrix.py

---
+++ Accepted/CountNegativeNumbersinaSortedMatrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countNegatives(self, grid: list[list[int]]) -> int:
+
+        counter = 0
+
+
+        for group in grid:
+            if min(group) > -1:
+                continue
+            else:
+                for num in group:
+                    if num < 0:
+                        counter+=1
+        return counter
+
+grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+s = Solution()
+print(s.countNegatives(grid))
